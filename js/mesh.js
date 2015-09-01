93d4e9b20695e58cbf615cc4f5083d51b3ad2fa7
robbiesh <sherrard.rob@gmail.com>
2015-09-01 13:52:04 -0400

    gn fix and hovers

--- js/mesh.js
+++ js/mesh.js
@@ -24,23 +24,24 @@ jQuery(document).ready(function($){
   $(document).scroll(function () {
     var y = $(this).scrollTop();
 
-
-    if (y > 300) {
-      $('.side-menu').css('position', 'fixed');
-      $('.side-menu').css('top', 50);
-    } else {
-      $('.side-menu').css('position', 'absolute');
-      $('.side-menu').css('top', $('.intro').position().top - 60);
+    if ( $('.side-menu').length ) {
+      if (y > 300) {
+        $('.side-menu').css('position', 'fixed');
+        $('.side-menu').css('top', 50);
+      } else {
+        $('.side-menu').css('position', 'absolute');
+        $('.side-menu').css('top', $('.intro').position().top - 60);
+      }
     }
 
-  });
 
+    $('.network-nav-item').click(function(event) {
+       var item = $(this).attr('id');
+       $('.network-filter-status').slideDown();
+       $('.network-filter').hide(); //Hide all dropdowns
+       $('.'+item).slideDown('400'); //Show this dropdowns
 
-  $('.network-nav-item').click(function(event) {
-     var item = $(this).attr('id');
-     $('.network-filter-status').slideDown();
-     $('.network-filter').hide(); //Hide all dropdowns
-     $('.'+item).slideDown('400'); //Show this dropdowns
+    });
 
   });
 
