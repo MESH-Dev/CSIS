8f05553656127b23c9e1c21d01db3dd629b32529
robbiesh <sherrard.rob@gmail.com>
2015-09-02 11:14:26 -0400

    form fill and remove default page

--- page.php
+++ page.php
@@ -4,22 +4,7 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="nine columns">
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-					<h1><?php the_title(); ?></h1>
-
-					<?php the_content(); ?>
-
-				<?php endwhile; ?>
-			</div>
-
-			<div class="three columns">
-
-				<!-- Change this to repeater of custom fields -->
-
-				<?php get_sidebar(); ?>
-			</div>
+			
 
 		</div>
 	</div>
