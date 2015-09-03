db9a523cbd8f2899792cbf2b120c799a870427d3
meshdesign <joshdodd@meshfresh.com>
2015-09-03 18:11:33 -0400

    read in csv and functions

--- functions.php
+++ functions.php
@@ -15,4 +15,112 @@ function my_custom_login_logo() {
 }
 
 
+function getCSVdata(){
+	$row = 1;
+
+      $csv = get_bloginfo('template_directory') . '/data/user-profiles.csv';
+     
+
+      if (($handle = fopen($csv, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+          $num = count($data);
+
+          if ($row == 1) {
+          	print_r($data);
+          }
+
+          if ($row != 1) {
+
+            for ($c=0; $c < $num; $c++) {
+                //echo $data[$c];
+            }
+
+            echo "<br/>";
+          }
+
+          $row++;
+        }
+        fclose($handle);
+      }
+}
+
+function generateJSON(){
+	//header('Content-type: application/json');
+	// Set your CSV feed
+	$feed = get_bloginfo('template_directory') . '/data/user-profiles.csv';
+	// Arrays we'll use later
+	$keys = array();
+	$newArray = array();
+	// Function to convert CSV into associative array
+	function csvToArray($file, $delimiter) { 
+	  if (($handle = fopen($file, 'r')) !== FALSE) { 
+	    $i = 0; 
+	    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) { 
+	      for ($j = 0; $j < count($lineArray); $j++) { 
+	        $arr[$i][$j] = $lineArray[$j]; 
+	      } 
+	      $i++; 
+	    } 
+	    fclose($handle); 
+	  } 
+	  return $arr; 
+	} 
+	// Do it
+	$data = csvToArray($feed, ',');
+	// Set number of elements (minus 1 because we shift off the first row)
+	$count = count($data) - 1;
+	  
+	//Use first row for names  
+	$labels = array_shift($data);  
+	foreach ($labels as $label) {
+	  $keys[] = $label;
+	}
+	// Add Ids, just in case we want them later
+	$keys[] = 'id';
+	for ($i = 0; $i < $count; $i++) {
+	  $data[$i][] = $i;
+	}
+	  
+	// Bring it all together
+	for ($j = 0; $j < $count; $j++) {
+	  $d = array_combine($keys, $data[$j]);
+	  $newArray[$j] = $d;
+	}
+
+	$jsonfile = get_bloginfo('template_directory') . '/data/user-profiles.json';
+
+
+	// Print it out as JSON
+	$fp = fopen('wp-content/themes/csis/data/user-profiles.json', 'w');
+	fwrite($fp, json_encode($newArray));
+	fclose($fp);
+ 
+}
+
+function getProfileArray(){
+	$json = get_bloginfo('template_directory') . '/data/user-profiles.json';
+	$string = file_get_contents($json);
+	$profiles = json_decode($string, true);
+
+	return $profiles;
+}
+
+function renderProfileGrid($profiles){
+
+ 	foreach ($profiles as $profile) {
+ 		echo $profile['Owner'] . '<br>'; 
+ 	}
+
+ 	
+}
+
+function getSingleProfile($profiles,$id){
+	//Loop here to generate single array 
+ 	return $profiles[$id]; 
+
+}
+
+
+
+
 ?>
--- templates/global-network.php
+++ templates/global-network.php
@@ -10,6 +10,15 @@ get_header(); ?>
 
       <?php
 
+      //generateJSON();
+      $profiles = getProfileArray();
+      //renderProfileGrid($profiles);
+      $profile = getSingleProfile($profiles,0);
+      echo $profile['Name | Last'];
+
+
+
+
       if (get_field('banner') == 'image') {
         $image = get_field('banner_image');
 
@@ -30,26 +39,7 @@ get_header(); ?>
         <?php
       }
 
-      $row = 1;
-      $csv = get_template_directory_uri() . '/data/data.csv';
-
-      if (($handle = fopen($csv, "r")) !== FALSE) {
-        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-          $num = count($data);
-
-          if ($row != 1) {
-
-            for ($c=0; $c < $num; $c++) {
-                echo $data[$c];
-            }
-
-            echo "<br/>";
-          }
-
-          $row++;
-        }
-        fclose($handle);
-      }
+      
 
       elseif (get_field('banner') == 'video') {
 
