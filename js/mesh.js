3d77259272f332a1925db3de3e17c7df89964b8d
robbiesh <sherrard.rob@gmail.com>
2015-09-15 17:55:02 -0400

    new tabs

--- js/mesh.js
+++ js/mesh.js
@@ -488,16 +488,7 @@ $('.callout-panel h2').hover(function () {
 });
 
 
-$('a').each(function() {
-   var a = new RegExp('/' + window.location.host + '/');
-   if(!a.test(this.href)) {
-       $(this).click(function(event) {
-           event.preventDefault();
-           event.stopPropagation();
-           window.open(this.href, '_blank');
-       });
-   }
-});
+
 
 // New tabs
 
