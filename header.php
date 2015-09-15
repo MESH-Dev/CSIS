483e8fef2d7aa1fe68781d039a5a9a3876d40168
robbiesh <sherrard.rob@gmail.com>
2015-09-15 17:58:37 -0400

    favicon

--- header.php
+++ header.php
@@ -44,6 +44,10 @@
 	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
 
+	<link rel="icon"
+      type="image/png" 
+      href="<?php echo get_template_directory_uri(); ?>/img/fav.png">
+
 	<?php wp_head(); ?>
 
 </head>
