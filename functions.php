3ddb03748ba3bf69e842329211860c4eea7c95e6
MESH <mesh@Megans-iMac.local>
2015-09-18 12:58:20 -0400

    responsive title on story page

--- functions.php
+++ functions.php
@@ -194,43 +194,43 @@ function createImages($profiles){
   $count = 0;
 
   foreach ($profiles as $profile){
-    
+
     //Get remote image location, filename and extension
-    $url = $profile['Profile Picture']; 
+    $url = $profile['Profile Picture'];
     $filename = basename($url);
     $ext = pathinfo($filename, PATHINFO_EXTENSION);
-     
+
 
     $fname = $profile['Name | First'];
     $fname = preg_replace("/[^a-zA-Z0-9\s]/", "", $fname);
     $fname = str_replace(' ', '', $fname);
- 
+
 
     $lname =  $profile['Name | Last'];
     $lname = preg_replace("/[^a-zA-Z0-9\s]/", "", $lname);
     $lname = str_replace(' ', '', $lname);
 
 
-    $location = 'wp-content/themes/csis/data/img/'.$fname . '-' . $lname.'.' .$ext; 
- 
+    $location = 'wp-content/themes/csis/data/img/'.$fname . '-' . $lname.'.' .$ext;
+
     if($url != '' && !(file_exists($location))){
       $imageString = file_get_contents($url);
       $fp = fopen($location, "w");
       fwrite($fp, $imageString);
       fclose($fp);
-      $new_loc = get_bloginfo('template_directory') . '/data/img/'.$fname . '-' . $lname.'.' .$ext; 
-      $profiles[$count]['Profile Picture'] = $new_loc; 
+      $new_loc = get_bloginfo('template_directory') . '/data/img/'.$fname . '-' . $lname.'.' .$ext;
+      $profiles[$count]['Profile Picture'] = $new_loc;
       createBWImage($location,$ext);
 
     }
     $count++;
-    
+
   }
   //print_r($profiles);
   file_put_contents('wp-content/themes/csis/data/user-profiles.json', json_encode($profiles));
 }
 
-function createBWImage($filename,$ext){ 
+function createBWImage($filename,$ext){
   if($ext == 'jpg'){
     $im = imagecreatefromjpeg($filename);
     if($im && imagefilter($im, IMG_FILTER_GRAYSCALE))
@@ -256,7 +256,7 @@ function createBWImage($filename,$ext){
         echo 'Conversion to grayscale failed.';
     }
     imagedestroy($im);
-  } 
+  }
 }
 
 
@@ -600,7 +600,7 @@ function saveGeneralEmail(){
   $headers = array('Content-Type: text/html; charset=UTF-8');
 
   wp_mail( $to, $subject, $body, $headers );
-
+  updateEmailDatabase()
   $output = "Thank you for signing up. You have been added to our mailing list!";
   echo $output;
 
@@ -631,7 +631,9 @@ function saveProductEmail(){
 
 }
 
+function updateEmailDatabase() {
 
+}
 
 
 
