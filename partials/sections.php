5edf9a8710ac9f36b9c216dbf5b010c0f5736d26
robbiesh <sherrard.rob@gmail.com>
2015-10-09 14:16:31 -0400

    email signup sections

--- partials/sections.php
+++ partials/sections.php
@@ -515,7 +515,7 @@ if( have_rows('sections') ):
 
             </div>
             <div class="four columns">
-              <form id="general-email" action="http://www2.socialimpactstrategy.org/l/121292/2015-10-07/5l6dbw" method="post">
+              <!-- <form id="general-email" action="http://www2.socialimpactstrategy.org/l/121292/2015-10-07/5l6dbw" method="post">
                 <span class="input input--hoshi">
                   <input class="input__field input__field--hoshi" type="email" id="email" name="email" spellcheck="false" />
                   <label class="input__label input__label--hoshi input__label--hoshi-color-1" for="email">
@@ -523,7 +523,10 @@ if( have_rows('sections') ):
                   </label>
                   <input type="hidden" id="ip" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" />
                 </span>
-              </form>
+              </form> -->
+
+              <iframe style="border: 0;" src="http://www2.socialimpactstrategy.org/l/121292/2015-10-08/5lq17k" width="100%" height="500" frameborder="0"></iframe>
+              
               <div id="loader" class="hidden">
                 <img src="<?php echo get_bloginfo("template_url" ); ?>/img/ajax-loader.gif" alt="">
               </div>
